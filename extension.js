22bc746f25e64332b3db369d0020c99bebb37981
Shadow-Slayer <wander.p.c.07@gmail.com>
2015-06-05 18:27:18 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -109,7 +109,7 @@
         fbLink: "http://goo.gl/gGsuJQ",
         youtubeLink: null,
         website: "http://goo.gl/O4tlVo",
-        intervalMessages: [":game_die: Hora de fazer a roleta! @staff :game_die:",":game_die: Faltam 2 musicas para a roleta",":game_die: Na roleta voce pode tanto ganhar posicoes, como tambem perder!!!"],
+        intervalMessages: [":game_die: Hora de fazer a roleta! @staff :game_die:"],
         messageInterval: 2,
         songstats: true,
         commandLiteral: "!",
