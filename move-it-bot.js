89e877d2ca05868e2f893c16bd1db332cb5bda0c
Shadow-Slayer <wander.p.c.07@gmail.com>
2015-06-07 23:14:32 -0300

    Update move-it-bot.js
    retirado comannd love

--- move-it-bot.js
+++ move-it-bot.js
@@ -1816,27 +1816,6 @@
                     }
                 }
             },
-
-            loveCommand: {
-                command: ['love', 'amor'],
-                rank: 'user',
-                type: 'startsWith',
-                getLove: function (chat) {
-                    var c = Math.floor(Math.random() * basicBot.chat.love.length);
-                    return basicBot.chat.love[c];
-                },
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);                    
-                        else {
-                            var name = msg.substring(space + 2);
-                            var user = basicBot.userUtilities.lookupUserName(name);
-		API.sendChat(subChat(basicBot.chat.love, {nameto: user.username, namefrom: chat.un, love: this.getlove()}));
-                            }
-                        }
-                    }
-                }
-            },
             cycleCommand: {
                 command: 'cycle',
                 rank: 'manager',
