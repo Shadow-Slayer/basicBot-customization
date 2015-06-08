4ca0d34ede402af8bed48db475f625b4c29fa80e
Shadow-Slayer <wander.p.c.07@gmail.com>
2015-06-07 23:49:02 -0300

    Update move-it-bot.js

--- move-it-bot.js
+++ move-it-bot.js
@@ -1382,21 +1382,6 @@
                         }
                 },
              **/
-
-            lovecommand: {
-                        command: 'love',
-                        rank: 'user',
-                        type: 'startsWith',
-                        functionality: function(chat, cmd){
-                                if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                                if( !basicBot.commands.executable(this.rank, chat) ) return void (0);
-                                else{
-					var user = basicBot.userUtilities.lookupUserName(name);
-                        		return API.sendChat(subChat(basicBot.chat.love, {nameto: user.username, namefrom: chat.un, love: this.getlove()}));
-                                
-                                }
-                        }
-            },
             activeCommand: {
                 command: 'active',
                 rank: 'bouncer',
@@ -1797,6 +1782,41 @@
 
             cookieCommand: {
                 command: ['cookie', 'biscoito'],
+                rank: 'user',
+                type: 'startsWith',
+                getCookie: function (chat) {
+                    var c = Math.floor(Math.random() * basicBot.chat.cookies.length);
+                    return basicBot.chat.cookies[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatcookie);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousercookie, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfcookie, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.cookie, {nameto: user.username, namefrom: chat.un, cookie: this.getCookie()}));
+                            }
+                        }
+                    }
+                }
+            },
+            loveCommand: {
+                command: 'love',
                 rank: 'user',
                 type: 'startsWith',
                 getCookie: function (chat) {
