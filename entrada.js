cf0f7d01b162b6236e0d6bf8b9e2f959ce5b0639
Shadow-Slayer <wander.p.c.07@gmail.com>
2015-06-11 09:18:30 -0300

    Create entrada.js

---
+++ entrada.js
@@ -0,0 +1,17 @@
+API.on(API.USER_JOIN, function(user) {
+     if (user.id == "id") {
+         API.sendChat("/me ooo @" + user.username + " oooo ");
+         }
+         
+      if (user.id == "id") {
+         API.sendChat("/me oo @" + user.username + " oo ");
+         } 
+		 
+      if (user.id == "id") {
+         API.sendChat("/me oo @" + user.username + " oo ");
+         }
+    
+     if (user.id == "id") {
+         API.sendChat("/me frase @" + user.username + " frase ");
+         }
+        });
