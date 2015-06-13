c72352d05a7acbd328a41626480d6b1c30719ae5
Shadow-Slayer <wander.p.c.07@gmail.com>
2015-06-13 01:54:47 -0300

    Update entrada.js

--- entrada.js
+++ entrada.js
@@ -2,7 +2,9 @@ API.on(API.USER_JOIN, function(user) {
      if (user.id == "4080795") {
          API.sendChat("/me gostosão @" + user.username + " chegooooou <3 ");
          }
-         
+      if (user.id == "4311237") {
+         API.sendChat("/me A ruiva mais linda entrou na sala @" + user.username + " <3 :heartpulse: ");
+         }    
       if (user.id == "id") {
          API.sendChat("/me oo @" + user.username + " oo ");
          } 
