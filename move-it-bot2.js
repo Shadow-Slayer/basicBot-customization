30cea5150eacaa08febb775400161ca28f9dbd00
Shadow-Slayer <wander.p.c.07@gmail.com>
2015-06-13 01:42:45 -0300

    Update move-it-bot2.js

--- move-it-bot2.js
+++ move-it-bot2.js
@@ -1601,24 +1601,6 @@
                     }
                 }
             },
-            loveCommand: {
-                command: ['love', 'ask'],
-                rank: 'user',
-                type: 'startsWith',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                            var crowd = API.getUsers();
-                            var msg = chat.message;
-                            var argument = msg.substring(cmd.length + 1);
-                            var randomUser = Math.floor(Math.random() * crowd.length);
-                            var randomBall = Math.floor(Math.random() * basicBot.chat.love.length);
-                            var randomSentence = Math.floor(Math.random() * 1);
-                            API.sendChat(subChat(basicBot.chat.love, {name: chat.un, response: basicBot.chat.love[randomBall]}));
-                     }
-                }
-            },
             ballCommand: {
                 command: ['8ball', 'ask'],
                 rank: 'user',
