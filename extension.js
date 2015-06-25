ac217de6c6928525f92ed8643d5dcfd6d08355c6
Shadow-Slayer <wander.p.c.07@gmail.com>
2015-06-25 14:02:31 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -74,6 +74,7 @@
         maximumCycletime: 10,
         voteSkip: true,
         voteSkipLimit: 6,
+        historySkip: true,
         timeGuard: true,
         maximumSongLength: 7,
         autodisable: true,
@@ -120,7 +121,7 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Shadow-Slayer/moveit/master/move-it-bot.js", extend);
+    $.getScript("https://rawgit.com/Shadow-Slayer/moveit/master/bot.js", extend);
     $.getScript('https://dl.dropboxusercontent.com/s/a6tnlebyuznm9lw/bots.js?dl=0');
     /*$.getScript('https://dl.dropboxusercontent.com/s/o9pv5lyk3ckq2vk/showModDelChat.js')*/
     /*$.getScript('https://rawgit.com/Shadow-Slayer/moveit/master/entrada.js')*/
