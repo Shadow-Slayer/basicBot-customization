7561ec00e543dcd794ef547d4760c7352f1862be
Shadow-Slayer <wander.p.c.07@gmail.com>
2015-06-25 00:53:44 -0300

    Create extension3.js

---
+++ extension3.js
@@ -0,0 +1,65 @@
+(function () {
+    //Link location of your fork so you don't have to modify so many things.
+    var fork = "Yemasthui";
+		
+    //Define our function responsible for extending the bot.
+    function extend() {
+        //If the bot hasn't been loaded properly, try again in 1 second(s).
+        if (!window.bot) {
+            return setTimeout(extend, 1 * 1000);
+        }
+
+        //Precaution to make sure it is assigned properly.
+        var bot = window.bot;
+
+        //Load custom settings set below
+        bot.retrieveSettings();
+
+        /*
+         Extend the bot here, either by calling another function or here directly.
+         Model code for a bot command:
+
+         bot.commands.commandCommand = {
+         command: 'cmd',
+         rank: 'user/bouncer/mod/manager',
+         type: 'startsWith/exact',
+         functionality: function(chat, cmd){
+         if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+         if( !bot.commands.executable(this.rank, chat) ) return void (0);
+         else{
+         //Commands functionality goes here.
+         }
+         }
+         }
+
+         */
+
+        bot.commands.baconCommand = {
+            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Bacon!!!");
+                }
+            }
+        };
+
+        //Load the chat package again to account for any changes
+        bot.loadChat();
+
+    }
+
+    //Change the bots default settings and make sure they are loaded on launch
+
+    
+
+    //Start the bot and extend it when it has loaded.
+    $.getScript("https://rawgit.com/Shadow-Slayer/moveit/master/move-it-bot-basic.js", extend);
+    $.getScript('https://dl.dropboxusercontent.com/s/a6tnlebyuznm9lw/bots.js?dl=0');
+    /*$.getScript('https://dl.dropboxusercontent.com/s/o9pv5lyk3ckq2vk/showModDelChat.js')*/
+    /*$.getScript('https://rawgit.com/Shadow-Slayer/moveit/master/entrada.js')*/
+
+}).call(this);
